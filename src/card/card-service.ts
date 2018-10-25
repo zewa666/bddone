da37888ab7075712cfed29501631ce2bc1fcfa4d
Vildan Softic <zewa666@gmail.com>
2018-10-25 17:28:12 +0200

    feat(autcomplete): switch to observables for live updates of list

--- src/card/card-service.ts
+++ src/card/card-service.ts
@@ -1,21 +1,20 @@
 import { autoinject } from "aurelia-framework";
 import { Store } from "aurelia-store";
+import { map, pluck } from "rxjs/operators";
 
+import { Card } from "../store/models";
 import { State } from "../store/state";
 
 export type GWT = "given" | "when" | "then";
 
 @autoinject()
 export class CardService {
-  private state: State;
-
-  constructor(private store: Store<State>) {
-    this.store.state.subscribe((s) => this.state = s);
-  }
+  constructor(private store: Store<State>) {}
 
   public getAvailableCompletions(type: GWT) {
-    return this.state.cards.map(
-      (c) => c[type]
-    ).sort();
+    return this.store.state.pipe(
+      pluck<State, Card[]>("cards"),
+      map((cards) => cards.map((c) => c.given))
+    );
   }
 }
--- src/components/awesomplete.ts
+++ src/components/awesomplete.ts
@@ -1,5 +1,6 @@
 import { customAttribute } from "aurelia-framework";
 import Awesomplete from "awesomplete";
+import { Subscription } from "rxjs";
 
 import { CardService, GWT } from "../card/card-service";
 
@@ -7,21 +8,33 @@ import { CardService, GWT } from "../card/card-service";
 export class Awesomeplete {
 
   private handle: Awesomplete;
+  private subscription: Subscription;
   private value: GWT;
 
   constructor(
     private element: Element,
     private cardService: CardService
-  ) {}
+  ) { }
 
-  public attached() {
+  public bind() {
     Object.defineProperty(this.element, "value", {
       get: () => (this.element as HTMLElement).innerText
     });
 
     this.handle = new Awesomplete(this.element, {
-      list: this.cardService.getAvailableCompletions(this.value || "given"),
+      list: [],
+      minChars: 1,
       replace: (text) => (this.element as HTMLElement).innerText = text
     });
+
+    this.subscription = this.cardService
+      .getAvailableCompletions(this.value || "given")
+      .subscribe((data) => {
+        this.handle.list = data;
+      });
+  }
+
+  public unbind() {
+    this.subscription.unsubscribe();
   }
 }
