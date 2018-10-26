adb1f37313c7ace8d29071f9aff38c0a9479ed9e
Vildan Softic <zewa666@gmail.com>
2018-10-26 22:47:25 +0200

    feat(toolbar): show no of cards and download

--- src/planning/overview.ts
+++ src/planning/overview.ts
@@ -1,3 +1,18 @@
-export class Overview {
+import { connectTo } from "aurelia-store";
+import { distinctUntilChanged } from "rxjs/operators";
 
+import { State } from "../store/state";
+
+@connectTo((store) => store.state.pipe(distinctUntilChanged()))
+export class Overview {
+  public state: State;
+  public btnExport: HTMLAnchorElement;
+
+  public stateChanged(newState) {
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(newState));
+
+    this.btnExport.setAttribute("href", dataStr);
+
+    return true;
+  }
 }
