7c896d08ec686662c9469e4c1d4c4092ec5c45d4
Vildan Softic <vsoftic@ranorex.com>
2018-10-29 14:43:16 +0100

    refactor(auth): leverage store for auth scenarios

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,6 +1,11 @@
 import { autoinject } from "aurelia-framework";
 import { AppRouter } from "aurelia-router";
+import { Store } from "aurelia-store";
 import auth0, { Auth0Error } from "auth0-js";
+import { pluck } from "rxjs/operators";
+
+import { loginUser, logoutUser } from "../store/actions";
+import { State } from "../store/state";
 
 @autoinject()
 export class AuthService {
@@ -14,7 +19,14 @@ export class AuthService {
 
   public user: auth0.Auth0UserProfile;
 
-  constructor(private router: AppRouter) { }
+  constructor(
+    private router: AppRouter,
+    private store: Store<State>
+  ) {
+    this.store.state.pipe(pluck<State, auth0.AdfsUserProfile>("user")).subscribe((u) => {
+      this.user = u;
+    });
+  }
 
   public login() {
     this.auth0.authorize();
@@ -23,7 +35,7 @@ export class AuthService {
   public handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
-        this.auth0.client.userInfo(authResult.accessToken, (uInfoErr: Auth0Error, user) => {
+        this.auth0.client.userInfo(authResult.accessToken, async (uInfoErr: Auth0Error, user) => {
           if (uInfoErr) {
             // tslint:disable-next-line:no-console
             console.error(uInfoErr);
@@ -33,7 +45,7 @@ export class AuthService {
           // This method will make a request to the /userinfo endpoint
           // and return the user object, which contains the user's information,
           // similar to the response below.
-          this.setSession(authResult, user);
+          await this.store.dispatch(loginUser, authResult, user);
 
           this.router.navigate("planning");
         });
@@ -44,23 +56,9 @@ export class AuthService {
     });
   }
 
-  public setSession(authResult: auth0.Auth0DecodedHash, user: auth0.Auth0UserProfile) {
-    // Set the time that the Access Token will expire at
-    const expiresAt = JSON.stringify(
-      authResult.expiresIn * 1000 + new Date().getTime()
-    );
-    localStorage.setItem("user", JSON.stringify(user));
-    localStorage.setItem("access_token", authResult.accessToken);
-    localStorage.setItem("id_token", authResult.idToken);
-    localStorage.setItem("expires_at", expiresAt);
-  }
-
-  public logout() {
+  public async logout() {
     // Clear Access Token and ID Token from local storage
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("id_token");
-    localStorage.removeItem("expires_at");
-    localStorage.removeItem("user");
+    await this.store.dispatch(logoutUser);
     this.router.navigate("planning");
   }
 
